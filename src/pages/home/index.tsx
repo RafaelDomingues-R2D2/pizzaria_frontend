b742c7e872a4c6f23b6f23737592882262d3b09e
r2d2 <rafael.dominguesr2d2@gmail.com>
2023-04-12 08:06:25 -0300

    Setup

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import { Container } from "./styles";
+import Head from "next/head";
+
+export default function Home() {
+    return (
+      <>
+        <Head>
+            <title>Home | Ignite Shop</title>
+        </Head>
+
+        <Container>Hello World</Container>
+      </>
+    )
+  };
+  
